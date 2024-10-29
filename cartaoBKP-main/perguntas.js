998ff51599c72a3b8fd8494a038c967ca80077df
n4thye <142032183+n4thye@users.noreply.github.com>
2024-10-29 11:01:49 -0300

    Add files via upload

---
+++ cartaoBKP-main/perguntas.js
@@ -0,0 +1,29 @@
+//variaveis
+
+//funções
+
+//chamada
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+
+criaCartao(
+    'Youtuber',
+    'Qual é a youtuber mais divonica de todes?',
+    'É a doll MC DIVERTIDA',
+)
