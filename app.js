ca99099b244c2f6f479896e5d9d166c99c5474e7
n4thye <142032183+n4thye@users.noreply.github.com>
2024-11-05 10:50:37 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function criaCartao(categoria,pergunta,resposta){
-
-let container = document.getElementById ("container")
-let cartao = document.createElement('article')
-cartao.className ='cartao'
-
-}
